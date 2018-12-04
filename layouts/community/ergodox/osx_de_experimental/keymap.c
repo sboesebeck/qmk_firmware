533f734c8804fd42dd8880817dde8caf321dc914
Stephan Bösebeck <sb@caluga.de>
2018-12-04 22:25:32 +0100

    re-adding mouse keys

--- layouts/community/ergodox/osx_de_experimental/keymap.c
+++ layouts/community/ergodox/osx_de_experimental/keymap.c
@@ -254,10 +254,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 **/
 [NUMB]=LAYOUT_ergodox(
 //left half
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, 
-    KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, 
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS,    KC_MS_BTN3,    KC_TRNS,    KC_TRNS,       KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2, KC_MS_WH_UP,   KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT,KC_MS_WH_DOWN, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_WH_LEFT, KC_MS_WH_RIGHT, 
                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
                                               KC_TRNS, KC_TRNS, 
                                                      KC_TRNS, 
