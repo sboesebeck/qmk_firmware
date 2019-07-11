e2d18ece51083a91031f195099a6e0517136c279
Stephan Bösebeck <stephan@boesebeck.biz>
2019-07-11 21:57:05 +0200

    tweaking settings for keyboard

--- layouts/community/ergodox/osx_de_experimental/config.h
+++ layouts/community/ergodox/osx_de_experimental/config.h
@@ -2,7 +2,11 @@
 #define CONFIG_USER_H
 
 #include QMK_KEYBOARD_CONFIG_H
-
+#undef TAPPING_TERM
+#undef ONESHOT_TIMEOUT
+#undef LEADER_TIMEOUT
+#define TAPPING_TERM 120
+#define ONESHOT_TIMEOUT 200
 #define LEADER_TIMEOUT 200 // leader key sequence timeout in millis
 //#define  AUTO_SHIFT_TIMEOUT 135
 #endif
