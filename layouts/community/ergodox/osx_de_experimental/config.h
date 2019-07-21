974eb8a3317004193b3b603c16e55bac9e92d511
Stephan Bösebeck <stephan@boesebeck.biz>
2019-07-21 21:46:35 +0200

    changing timings

--- layouts/community/ergodox/osx_de_experimental/config.h
+++ layouts/community/ergodox/osx_de_experimental/config.h
@@ -5,7 +5,7 @@
 #undef TAPPING_TERM
 #undef ONESHOT_TIMEOUT
 #undef LEADER_TIMEOUT
-#define TAPPING_TERM 120
+#define TAPPING_TERM 150
 #define ONESHOT_TIMEOUT 200
 #define LEADER_TIMEOUT 200 // leader key sequence timeout in millis
 //#define  AUTO_SHIFT_TIMEOUT 135
